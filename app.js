f9a1d22419c7ebede93e3edaccea54ba14278de1
GunaUI <linkguna@gamil.com>
2019-10-26 06:42:21 +0530

    Code enhancing

--- app.js
+++ app.js
@@ -1,54 +1,24 @@
-// const http = require('http');
+const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
+
 const errorController = require('./controllers/error');
 
-const app = express()
+const app = express();
 
 app.set('view engine', 'ejs');
-
 app.set('views', 'views');
 
-const adminRouter = require("./routes/admin");
-const shopRouter = require("./routes/shop");
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static(path.join(__dirname,'public')))
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-//app.use("/admin",adminRouter); since we changed the way we export adminRouter we are using the below one.
-//app.use("/admin",adminRouter.routes); since we changed the way we export adminRouter again we removed this way of export
-app.use("/admin",adminRouter);
-
-app.use(shopRouter);
 app.use(errorController.get404);
-
-// app.use((req, res, next)=>{
-//     console.log("In the middleware!!");
-//     next(); //Allow the request to travel on to the next middleware in line
-// })
-// app.use('/add-product',(req, res, next)=>{
-//     console.log("In the add-product middleware!!")
-//     res.send('<h1>Hello From Express- add-product</h1>')
-// })
-
-// app.use('/add-product',(req, res, next)=>{
-//     res.send('<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Add Product</button></form>');
-// })
-
-// app.post('/product',(req, res, next)=>{
-//     console.log(req.body)
-//     res.redirect('/');
-// })
-
-// app.use('/',(req, res, next)=>{
-//     res.send('<h1>Hello From Express</h1>');
-// })
-
-// const server = http.createServer(app);      *** Not really need we could do simple as below
-
-// server.listen(8000);
 
 app.listen(8000);
--- models/product.js
+++ models/product.js
@@ -1,38 +1,40 @@
-
 const fs = require('fs');
 const path = require('path');
-const rootDir = require('../util/path');
-const filepath = path.join(
-    rootDir,
-    'data',
-    'products.json'
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
 );
 
-const getProductsFromFile = (cb) =>{
-    fs.readFile(filepath, (error, fileContent) =>{
-        if(error){
-            cb([])
-        }else{
-            cb(JSON.parse(fileContent));
-        }
-        
-    })
-}
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
 
 module.exports = class Product {
-    constructor(thalaipu){
-        this.title = thalaipu;
-    }
-    save() {
-        getProductsFromFile(products => {
-            products.push(this);
-            //stringify() takes javascript object/ array and convert that key into string.
-            fs.writeFile(filepath, JSON.stringify(products), error=>{
-                console.log(error);
-            });
-        })
-    }
-    static fetchAll(cb){ // static keyword which javascript offers which makes sure that I can call this method directly on the class itself and not on an instantiated object
-        getProductsFromFile(cb)
-    }
-}
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
+
+  save() {
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+};
